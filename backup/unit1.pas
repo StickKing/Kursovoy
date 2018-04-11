cdddca8833eba7708f5b941a3789569c7af89ac2
StickKing <32791930+StickKing@users.noreply.github.com>
2018-04-11 15:44:28 +0300

    Interface
    Готов визуал, просто внешний вид.

---
+++ backup/unit1.pas
@@ -0,0 +1,69 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+    Label1: TLabel;
+    Label10: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    teory: TGroupBox;
+    test: TGroupBox;
+    menu_: TGroupBox;
+    test1: TGroupBox;
+    test2: TGroupBox;
+    test3: TGroupBox;
+    test4: TGroupBox;
+    test5: TGroupBox;
+    test6: TGroupBox;
+    test7: TGroupBox;
+    test8: TGroupBox;
+    procedure Button2Click(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+end.
+
